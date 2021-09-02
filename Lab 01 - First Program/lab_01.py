eadac06e62f54ec33648a5a8cde5ee2c46aa7444
Morgan Castenson <morgan.castenson@my.simpson.edu>
2021-09-02 15:27:15 -0500

    Work on Lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,10 @@
+print("I have three sibilings and thier names are Kelsey, Curt, and Renee.")
+print("I enjoy \ngoing to \nsporting events in \nmy free time!!")
+print("I am majoring in Elementary Education and am a Junior here at Simpson!")
+print("""I have 
+three
+in-laws
+in my
+family and thier names are
+John, Nate, and Annie.
+""")
