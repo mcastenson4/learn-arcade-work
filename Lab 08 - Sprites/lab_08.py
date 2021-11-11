eaca87bc4ffc95906b3460a7794eb604a852418c
Morgan Castenson <morgan.castenson@my.simpson.edu>
2021-11-11 15:12:02 -0600

    Lab 8

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -1,134 +1,278 @@
-import random
 import arcade
+import random
 import math
 
-SPRITE_SCALING = 0.5
+# --- Constants ---
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+MOVEMENT_SPEED = 7
+ROBOT_SCALE = 0.5
+SLIME_SCALE = 0.40
+SLIME_COUNT = 40
+METEOR_SCALE = 0.40
+METEOR_COUNT = 40
+STAR_SPEED = 40
+STAR_SCALE = 1
+BALL_SCALE = 0.05
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Lab 8 - Sprites"
 
-VIEWPOINT_MARGIN = 20
-MOVEMENT_SPEED = 12
-
-class Coin(arcade.Sprite):
+class Slime(arcade.Sprite):
 
     def __init__(self, filename, sprite_scaling):
-        """ Constructor. """
+
         super().__init__(filename, sprite_scaling)
 
-        self.circle_angle = 0
-
-        self.circle_radius = 0
-
-        self.circle_speed = 0.008
-
-        self.circle_center_x = 0
-        self.circle_center_y = 0
+        self.change_x = 0
+        self.change_y = 0
 
     def update(self):
 
-        """ Update the ball's position. """
-        # Calculate a new x, y
-        self.center_x = self.circle_radius * math.sin(self.circle_angle) \
-            + self.circle_center_x
-        self.center_y = self.circle_radius * math.cos(self.circle_angle) \
-            + self.circle_center_y
+        self.center_x += self.change_x
+        self.center_y += self.change_y
 
-        # Increase the angle in prep for the next round.
-        self.circle_angle += self.circle_speed
+        if self.left < 0:
+            self.change_x *= -1
+
+        if self.right > SCREEN_WIDTH:
+            self.change_x *= -1
+
+        if self.bottom < 0:
+            self.change_y *= -1
+
+        if self.top > SCREEN_HEIGHT:
+            self.change_y *= -1
+
+
+class Meteor(arcade.Sprite):
+
+    def __init__(self, filename, sprite_scaling):
+
+        super().__init__(filename, sprite_scaling)
+
+        self.change_x = 0
+        self.change_y = 0
+
+    def update(self):
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.change_x *= -1
+
+        if self.right > SCREEN_WIDTH:
+            self.change_x *= -1
+
+        if self.bottom < 0:
+            self.change_y *= -1
+
+        if self.top > SCREEN_HEIGHT:
+            self.change_y *= -1
 
 
 class MyGame(arcade.Window):
-    """ Main application class. """
 
-    def __init__(self, width, height):
+    def __init__(self):
 
-        super().__init__(width, height)
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 8 Sprites")
 
         # Sprite lists
         self.robot_list = None
         self.slime_list = None
+        self.meteor_list = None
+        self.star_list = None
+        self.ball_list = None
 
-        # Set up the player
-        self.score = 0
+        # robot Info
         self.robot_sprite = None
-
-    def start_new_game(self):
-        """ Set up the game and initialize the variables. """
-
-        # Sprite lists
-        self.robot_list = arcade.SpriteList()
-        self.slime_list = arcade.SpriteList()
-
-        # Set up the player
         self.score = 0
 
-        # Character image from kenney.nl
-        self.robot_sprite = arcade.Sprite("robot_jump.png", SPRITE_SCALING)
-        self.robot_sprite.center_x = 50
-        self.robot_sprite.center_y = 70
-        self.robot_list.append(self.robot_sprite)
+        self.ball_sprite = None
 
-        for i in range(50):
+        self.gun_sound = arcade.load_sound("arcade_resources_sounds_error2.wav")
+        self.good_hit_sound = arcade.sound.load_sound("arcade_resources_sounds_explosion1.wav")
+        self.bad_hit_sound = arcade.sound.load_sound("arcade_resources_sounds_rockHit2.wav")
 
-            slime = Slime("slimePurple.png", SPRITE_SCALING / 3)
+        self.time_taken = 0
+        arcade.set_background_color(arcade.color.CADET_BLUE)
 
-            slime.circle_center_x = random.randrange(SCREEN_WIDTH)
-            slime.circle_center_y = random.randrange(SCREEN_HEIGHT)
-
-            # Random radius from 10 to 200
-            slime.circle_radius = random.randrange(10, 200)
-
-            # Random start angle from 0 to 2pi
-            slime.circle_angle = random.random() * 2 * math.pi
-
-            # Add the coin to the lists
-            self.slime_list.append(slime)
-
-        # Don't show the mouse cursor
         self.set_mouse_visible(False)
 
-        # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
+    def setup(self):
+
+        # Sprite Lists
+        self.robot_list = arcade.SpriteList()
+        self.slime_list = arcade.SpriteList()
+        self.meteor_list = arcade.SpriteList()
+        self.star_list = arcade.SpriteList()
+        self.ball_list = arcade.SpriteList()
+
+        # Score
+        self.score = 0
+
+        # Set up Robot
+        # Character image from api.arcade.academy in Python Arcade Library Resources
+        self.robot_sprite = arcade.Sprite("robot_jump.png", ROBOT_SCALE, flipped_horizontally=True)
+        self.robot_sprite.center_x = 50
+        self.robot_sprite.center_y = 50
+        self.robot_list.append(self.robot_sprite)
+
+        # BAll image from clipart-library.com in Clipart Library
+        self.ball_sprite = arcade.Sprite("pool_cue_ball.png", BALL_SCALE)
+        self.ball_sprite.center_x = 60
+        self.ball_sprite.center_y = 60
+        self.ball_list.append(self.ball_sprite)
+
+        # Create the Slime
+        for i in range(SLIME_COUNT):
+
+            # slime image from api.arcade.academy in Python Arcade Library Resources
+            slime = Slime("slimePurple.png", SLIME_SCALE)
+
+            slime.center_x = random.randrange(SCREEN_WIDTH)
+            slime.center_y = random.randrange(120, SCREEN_HEIGHT)
+            slime.change_x = random.randrange(-3, 4)
+            slime.change_y = random.randrange(-3, 4)
+
+            self.slime_list.append(slime)
+
+        for i in range(METEOR_COUNT):
+
+            # Meteor image from api.arcade.academy in Python Arcade Library Resources
+            meteor = Meteor("meteorGrey_big4.png", METEOR_SCALE)
+
+            meteor.center_x = random.randrange(SCREEN_WIDTH)
+            meteor.center_y = random.randrange(120, SCREEN_HEIGHT)
+            meteor.change_x = random. randrange(-3, 4)
+            meteor.change_y = random. randrange(-3, 4)
+
+            self.meteor_list.append(meteor)
 
     def on_draw(self):
 
-        # This command has to happen before we start drawing
         arcade.start_render()
-
-        # Draw all the sprites.
-        self.slime_list.draw()
         self.robot_list.draw()
+        self.ball_list.draw()
+        self.slime_list.draw()
+        self.meteor_list.draw()
+        self.star_list.draw()
 
-        # Put the text on the screen.
-        output = "Score: " + str(self.score)
-        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+        output = f"score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 30)
+
+        if len(self.slime_list) == 0:
+            arcade.draw_text("Game Over",
+                             SCREEN_WIDTH / 2,
+                             SCREEN_HEIGHT / 2,
+                             arcade.color.WHITE, 80,
+                             anchor_x="center")
 
     def on_mouse_motion(self, x, y, dx, dy):
-        self.robot_sprite.center_x = x
-        self.robot_sprite.center_y = y
+        if len(self.slime_list) != 0:
+            self.ball_sprite.center_x = x
+            self.ball_sprite.center_y = y
+        else:
+            self.set_mouse_visible(True)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if len(self.meteor_list) != 0:
+            # Star image from api.arcade.academy in Python Arcade Library Resources
+            star = arcade.Sprite("star.png", STAR_SCALE)
+
+            start_x = self.robot_sprite.center_x
+            start_y = self.robot_sprite.center_y + 30
+            star.center_x = start_x
+            star.center_y = start_y
+
+            destination_x = x
+            destination_y = y
+
+            x_diff = destination_x - start_x
+            y_diff = destination_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            star.change_x = math.cos(angle) * STAR_SPEED
+            star.change_y = math.sin(angle) * STAR_SPEED
+
+            self.star_list.append(star)
+            arcade.play_sound(self.gun_sound)
 
     def update(self, delta_time):
-        """ Movement and game logic """
+        if len(self.slime_list) != 0:
+            self.slime_list.update()
+            self.meteor_list.update()
+            self.star_list.update()
+            self.robot_sprite.update()
 
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
-        self.slime_list.update()
+            good_hit_list = arcade.check_for_collision_with_list(self.robot_sprite, self.slime_list)
+            bad_hit_list = arcade.check_for_collision_with_list(self.robot_sprite, self.meteor_list)
 
-        # Generate a list of all sprites that collided with the player.
-        hit_list = arcade.check_for_collision_with_list(self.robot_sprite,
-                                                        self.slime_list)
+            for slime in good_hit_list:
+                slime.remove_from_sprite_lists()
+                self.score += 1
+                arcade.play_sound(self.good_hit_sound)
 
-        # Loop through each colliding sprite, remove it, and add to the score.
-        for slime in hit_list:
-            self.score += 1
-            slime.remove_from_sprite_lists()
+            for meteor in bad_hit_list:
+                meteor.remove_from_sprite_lists()
+                self.score -= 1
+                arcade.play_sound(self.bad_hit_sound)
+
+            for star in self.star_list:
+
+                good_hit_list = arcade.check_for_collision_with_list(star, self.slime_list)
+                bad_hit_list = arcade.check_for_collision_with_list(star, self.meteor_list)
+
+                if len(good_hit_list) > 0:
+                    star.remove_from_sprite_lists()
+                for slime in good_hit_list:
+                    slime.remove_from_sprite_lists()
+                    self.score += 1
+                    arcade.play_sound(self.good_hit_sound)
+
+                if len(bad_hit_list) > 0:
+                    star.remove_from_sprite_lists()
+                for meteor in bad_hit_list:
+                    meteor.remove_from_sprite_lists()
+                    self.score -= 1
+                    arcade.play_sound(self.bad_hit_sound)
+
+                if star.bottom > self.width or\
+                        star.top < 0 or\
+                        star.right < 0 or\
+                        star.left > self.width:
+                    star.remove_from_sprite_lists()
+
+            if self.robot_sprite.top > SCREEN_HEIGHT:
+                self.robot_sprite.top = SCREEN_HEIGHT
+            if self.robot_sprite.left < 0:
+                self.robot_sprite.left = 0
+            if self.robot_sprite.bottom < 0:
+                self.robot_sprite.bottom = 0
+            if self.robot_sprite.right > SCREEN_WIDTH:
+                self.robot_sprite.right = SCREEN_WIDTH
+
+    def on_key_press(self, key, modifiers):
+
+        if key == arcade.key.A:
+            self.robot_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.D:
+            self.robot_sprite.change_x = MOVEMENT_SPEED
+        elif key == arcade.key.W:
+            self.robot_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.S:
+            self.robot_sprite.change_y = -MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.A or key == arcade.key.D:
+            self.robot_sprite.change_x = 0
+        elif key == arcade.key.W or key == arcade.key.S:
+            self.robot_sprite.change_y = 0
 
 
 def main():
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
-    window.start_new_game()
+    window = MyGame()
+    window.setup()
     arcade.run()
 
 
